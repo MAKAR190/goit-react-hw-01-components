206d0ddcd6b8c604c435584930103690bccc9195
unknown <lutskyimakar@gmail.com>
2020-09-14 21:17:57 +0300

    added task 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
-import Profile from './Profile/Profile';
+import React from "react";
+import "./App.css";
+import Profile from "./Profile/Profile";
 import user from "./Profile/user.json";
+import Statistics from "./Statistics/Statistics";
+import statisticsData from "./Statistics/statistics-data.json";
 function App() {
   return (
-    <Profile name={user.name} tag={user.tag} avatar={user.avatar} location={user.location} stats={user.stats} />
+    <div className="app">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        avatar={user.avatar}
+        location={user.location}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticsData} />
+    </div>
   );
 }
 
---
+++ src/Statistics/Statistics.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Statistics.module.css";
+import PropTypes from "prop-types";
+function Statistics(props) {
+  return (
+    <section className={styles.statistics}>
+      <h2 className={styles.title}>{props.title}</h2>
+      <ul className={styles.list}>
+        {props.stats.map((obj) => (
+          <li key={obj.id}>
+            <span>{obj.label}</span>
+            <span className={styles.num}>{obj.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
+};
+export default Statistics;
